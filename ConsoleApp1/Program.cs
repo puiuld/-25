a8b811e75cfaf8396db5d20bc4cfaa2249046aa6
Даниил <puiuld47@gmail.com>
2025-02-16 22:22:50 +0200

    Task2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[] {9,3,2,6,5};
+            foreach (int el in omas)
+            {
+                Console.Write($"{el}\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}

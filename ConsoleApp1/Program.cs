60fa09136c6affed970068b3454b7f8d7d453f97
Даниил <puiuld47@gmail.com>
2025-02-16 22:20:07 +0200

    Task4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[6];
+            Random unt = new Random();
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = unt.Next(-10, 10);
+            }
+            foreach (int el in omas)
+            {
+                Console.Write($"{el}\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}

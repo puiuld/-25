bec07b7316c431268301b9420ab3918745e80621
Даниил <puiuld47@gmail.com>
2025-02-16 21:53:50 +0200

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"omas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"omas[{i}]={omas[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
